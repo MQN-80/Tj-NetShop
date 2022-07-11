6e6946071fffc4b17cc031021b57ccfeb895c40b
梦秋年 <oncwnuFi0ujwfi5-C_5zg7Z0IblM@git.weixin.qq.com>
2022-07-11 11:00:56 +0800

    7-11-update

--- Models/dataAcess.cs
+++ Models/dataAcess.cs
@@ -1,85 +1,1 @@
-﻿using Oracle.ManagedDataAccess.Client;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebApi.Models
-{
-    public class dataAcess
-    {
-            public static OracleConnection DB;
-
-            //建立数据库连接
-            public static void CreateConn()  //更改此处数据库地址即可
-            {
-            //172.17.0.2
-            string user = "shop";
-                string pwd = "jinyi123mx427";
-                string db = "172.17.0.2/helowin";
-                string conStringUser = "User ID=" + user + ";password=" + pwd + ";Data Source=" + db + ";";
-                //string connString = "Data Source=(DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = orcl))); Persist Security Info=True;User ID=c##shop;Password=jinyi123mx427;";
-                DB = new OracleConnection(conStringUser);
-                DB.Open();
-            }
-
-            //关闭数据库连接
-            public static void CloseConn()
-            {
-                DB.Close();
-            }
-
-            //与功能点1：登录与注册相关的操作
-
-            //在MUser表中查询用户、密码是否错误(登录时使用)
-            //密码或用户名错误返回false；密码和用户名正确返回true
-            public static bool IsUserExist(string UserID, string Password)
-            {
-                int Count;
-                OracleCommand CMD = DB.CreateCommand();
-                CMD.CommandText = "select count(*) from user_info where user_id=:UserID and password=:Password";
-                CMD.Parameters.Add(new OracleParameter(":UserID", UserID));
-                CMD.Parameters.Add(new OracleParameter(":Password", Password));
-                Count = Convert.ToInt32(CMD.ExecuteScalar());
-                CloseConn();
-                if (Count == 0)
-                    return false;
-                else
-                    return true;
-
-            }
-
-            //向MUser表中增加一个新用户(注册)
-            //添加成功返回UserID，添加失败返回“0”
-            public static string AddUser(string UserName, string UserPassword)
-            {
-                OracleCommand Insert = DB.CreateCommand();
-                Insert.CommandText = "insert into user_info values(:UserName,:UserPassword)";
-                Insert.Parameters.Add(new OracleParameter(":UserName", UserName));
-                Insert.Parameters.Add(new OracleParameter(":UserPassword", UserPassword));
-                int Result = Insert.ExecuteNonQuery();
-                CloseConn();
-                if (Result == 1)
-                {
-                    return UserName;
-                }
-                else return "0";
-            }
-
-            //查找个人信息
-            public static User FindUserInfo(string UserID)
-            {
-                User U = new User();
-                OracleCommand Search = DB.CreateCommand();
-                Search.CommandText = "select * from user_info where user_id=:UserID";
-                Search.Parameters.Add(new OracleParameter(":UserID", UserID));
-                OracleDataReader Ord = Search.ExecuteReader();
-                while (Ord.Read())
-                {
-                    U.UserID = UserID;
-                    U.UserPassword = Ord.GetValue(1).ToString();
-                }
-                return U;
-            }
-        }
-}
+using Oracle.ManagedDataAccess.Client;using System;using System.Collections.Generic;using System.Linq;using System.Threading.Tasks;namespace WebApi.Models{    public class dataAcess    {            public static OracleConnection DB;            //建立数据库连接            public static void CreateConn()  //更改此处数据库地址即可            {            //172.17.0.2            string user = "shop";                string pwd = "jy2051914";                string db = "124.222.1.19/helowin";                string conStringUser = "User ID=" + user + ";password=" + pwd + ";Data Source=" + db + ";";                DB = new OracleConnection(conStringUser);                DB.Open();            }            //关闭数据库连接            public static void CloseConn()            {                DB.Close();            }            //与功能点1：登录与注册相关的操作            //在MUser表中查询用户、密码是否错误(登录时使用)            //密码或用户名错误返回false；密码和用户名正确返回true            public static bool IsUserExist(string UserID, string Password)            {                int Count;                OracleCommand CMD = DB.CreateCommand();                CMD.CommandText = "select count(*) from user_info where user_id=:UserID and password=:Password";                CMD.Parameters.Add(new OracleParameter(":UserID", UserID));                CMD.Parameters.Add(new OracleParameter(":Password", Password));                Count = Convert.ToInt32(CMD.ExecuteScalar());                CloseConn();                if (Count == 0)                    return false;                else                    return true;            }            //向MUser表中增加一个新用户(注册)            //添加成功返回UserID，添加失败返回“0”            public static string AddUser(string UserName, string UserPassword)            {                OracleCommand Insert = DB.CreateCommand();                Insert.CommandText = "insert into user_info values(:UserName,:UserPassword)";                Insert.Parameters.Add(new OracleParameter(":UserName", UserName));                Insert.Parameters.Add(new OracleParameter(":UserPassword", UserPassword));                int Result = Insert.ExecuteNonQuery();                CloseConn();                if (Result == 1)                {                    return UserName;                }                else return "0";            }            //查找个人信息            public static User FindUserInfo(string UserID)            {                User U = new User();                OracleCommand Search = DB.CreateCommand();                Search.CommandText = "select * from user_info where user_id=:UserID";                Search.Parameters.Add(new OracleParameter(":UserID", UserID));                OracleDataReader Ord = Search.ExecuteReader();                while (Ord.Read())                {                    U.UserID = UserID;                    U.UserPassword = Ord.GetValue(1).ToString();                }                return U;            }        }}
