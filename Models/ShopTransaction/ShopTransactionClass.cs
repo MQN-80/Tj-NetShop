897a2aa4b63f28202d08602bea3f605be1b3b4d5
chizhan9527 <1346885811@qq.com>
2022-08-10 18:46:51 +0800

    商品交易后台
    获得用户所有收货地址

---
+++ Models/ShopTransaction/ShopTransactionClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models.ShopTransaction
+{
+  public class Delivery_address
+  {
+    public string Id { get; set; }
+    public string User_id { get; set; }
+    public string Addr { get; set; }
+    public string Phone_number { get; set; }
+    public string Name { get; set; }
+    public string Add_default { get; set; }
+  }
+
+}
