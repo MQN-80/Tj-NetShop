e2b32aa0f38b41ea7e6dc58fd31c6e4f7b55b155
DESKTOP-FSA0CO7\Giantgoat <2185678515@qq.com>
2022-08-22 18:51:15 +0800

    shopcenter模块的最近热销功能完成

---
+++ Controllers/ShopCenter/ShopCenterController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+using WebApi.Models.ShopCenter;
+
+namespace WebApi.Controllers.ShopCenter
+{
+    public class ShopTransactionController
+    {
+        /*
+         * 返回四个随机商品
+         */
+        [Route("/ShopCenter/get_delivery_address")]
+        [HttpGet]
+        public string get_4_random_product()
+        {
+            ShopCenterModel shopCenterModel = new ShopCenterModel();
+            return shopCenterModel.Get_4_Random_Product();
+        }
+       
+    }
+}
